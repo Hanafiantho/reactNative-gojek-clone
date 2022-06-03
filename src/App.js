69d11bb110acef1c0a3a93c7d97f26ba966cef25
hanafi@chata.id <hanafi@chata.id>
2022-06-03 21:40:18 +0700

    news and voucher section

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import {
   View,
   TextInput,
   Image,
+  TouchableOpacity,
 } from 'react-native';
 
 import {
@@ -56,229 +57,246 @@ const App: () => Node = () => {
           </View>
         </View>
 
-        <View style={{paddingHorizontal: 18, marginBottom: 18}}>
-          <View style={styles.headerGopayContainer}>
-            <Image source={require('./assets/icon/gopay.png')} />
-            <Text style={styles.gopaySaldoText}>Rp 100.000</Text>
+        <ScrollView>
+          <View style={{paddingHorizontal: 18, marginBottom: 18}}>
+            <View style={styles.headerGopayContainer}>
+              <Image source={require('./assets/icon/gopay.png')} />
+              <Text style={styles.gopaySaldoText}>Rp 100.000</Text>
+            </View>
+            <View style={styles.bodyGopayContainer}>
+              <View style={styles.gopayIconContainer}>
+                <Image source={require('./assets/icon/pay.png')} />
+                <Text style={styles.gopayIconText}>Pay</Text>
+              </View>
+              <View style={styles.gopayIconContainer}>
+                <Image source={require('./assets/icon/nearby.png')} />
+                <Text style={styles.gopayIconText}>Nearby</Text>
+              </View>
+              <View style={styles.gopayIconContainer}>
+                <Image source={require('./assets/icon/topup.png')} />
+                <Text style={styles.gopayIconText}>Top Up</Text>
+              </View>
+              <View style={styles.gopayIconContainer}>
+                <Image source={require('./assets/icon/more.png')} />
+                <Text style={styles.gopayIconText}>More</Text>
+              </View>
+            </View>
           </View>
-          <View style={styles.bodyGopayContainer}>
-            <View style={styles.gopayIconContainer}>
-              <Image source={require('./assets/icon/pay.png')} />
-              <Text style={styles.gopayIconText}>Pay</Text>
-            </View>
-            <View style={styles.gopayIconContainer}>
-              <Image source={require('./assets/icon/nearby.png')} />
-              <Text style={styles.gopayIconText}>Nearby</Text>
-            </View>
-            <View style={styles.gopayIconContainer}>
-              <Image source={require('./assets/icon/topup.png')} />
-              <Text style={styles.gopayIconText}>Top Up</Text>
-            </View>
-            <View style={styles.gopayIconContainer}>
-              <Image source={require('./assets/icon/more.png')} />
-              <Text style={styles.gopayIconText}>More</Text>
-            </View>
-          </View>
-        </View>
 
-        <View>
-          <View style={{flexDirection: 'row', marginBottom: 18}}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View
-                style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
-                }}>
-                <Image source={require('./assets/icon/go-ride.png')} />
+          <View style={{marginBottom: 20}}>
+            <View style={{flexDirection: 'row', marginBottom: 18}}>
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-ride.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>GO-RIDE</Text>
               </View>
-              <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                GO-RIDE
-              </Text>
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-car.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>GO-CAR</Text>
+              </View>
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-bluebird.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>GO-BLUEBIRD</Text>
+              </View>
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-send.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>GO-SEND</Text>
+              </View>
             </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View
-                style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
-                }}>
-                <Image source={require('./assets/icon/go-car.png')} />
+
+            <View style={{flexDirection: 'row'}}>
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-deals.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>GO-DEALS</Text>
               </View>
-              <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                GO-CAR
-              </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View
-                style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
-                }}>
-                <Image source={require('./assets/icon/go-bluebird.png')} />
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-pulsa.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>GO-PULSA</Text>
               </View>
-              <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                GO-BLUEBIRD
-              </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View
-                style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
-                }}>
-                <Image source={require('./assets/icon/go-send.png')} />
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-food.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>GO-FOOD</Text>
               </View>
-              <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                GO-SEND
-              </Text>
+              <View style={styles.featureContainer}>
+                <View style={styles.featureIconBox}>
+                  <Image source={require('./assets/icon/go-more.png')} />
+                </View>
+                <Text style={styles.featureTextStyle}>MORE</Text>
+              </View>
             </View>
           </View>
-          <View style={{flexDirection: 'row'}}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+
+          <View style={{height: 17, backgroundColor: '#F2F2F4'}}></View>
+
+          <View style={{padding: 16}}>
+            <View style={{marginBottom: 16, position: 'relative'}}>
+              <Image
+                source={require('./assets/dummy/sepak-bola.jpg')}
+                style={{width: '100%', height: 170, borderRadius: 7}}
+              />
               <View
                 style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
+                  backgroundColor: 'black',
+                  width: '100%',
+                  height: 170,
+                  borderRadius: 7,
+                  opacity: 0.2,
+                  position: 'absolute',
+                }}></View>
+              <View
+                style={{
+                  position: 'absolute',
+                  height: 15,
+                  width: 60,
+                  top: 16,
+                  left: 16,
                 }}>
-                <Image source={require('./assets/icon/go-deals.png')} />
+                <Image
+                  source={require('./assets/logo/white.png')}
+                  style={{
+                    width: undefined,
+                    height: undefined,
+                    resizeMode: 'contain',
+                    flex: 1,
+                  }}
+                />
               </View>
-              <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                GO-DEALS
-              </Text>
             </View>
             <View
               style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
+                borderBottomWidth: 1,
+                borderColor: '#E8E9ED',
+                paddingBottom: 16,
               }}>
-              <View
-                style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
-                }}>
-                <Image source={require('./assets/icon/go-pulsa.png')} />
-              </View>
               <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                GO-PULSA
+                style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C'}}>
+                GO-NEWS
               </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View
-                style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
-                }}>
-                <Image source={require('./assets/icon/go-food.png')} />
-              </View>
               <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                GO-FOOD
-              </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <View
                 style={{
-                  borderWidth: 1,
-                  width: 58,
-                  height: 58,
-                  borderColor: '#EFEFEF',
-                  borderRadius: 18,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginBottom: 6,
+                  fontSize: 14,
+                  fontWeight: 'normal',
+                  color: '#7A7A7A',
+                  marginBottom: 11,
                 }}>
-                <Image source={require('./assets/icon/go-more.png')} />
-              </View>
-              <Text
-                style={{fontSize: 11, fontWeight: 'bold', textAlign: 'center'}}>
-                MORE
+                Dimas Dirajat selamatkan penalti, Timnas U-23 kalahkan Brunei
               </Text>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#61A756',
+                  paddingHorizontal: 12,
+                  paddingVertical: 11,
+                  borderRadius: 4,
+                  alignSelf: 'flex-end',
+                }}>
+                <Text
+                  style={{color: 'white', fontSize: 13, fontWeight: 'bold'}}>
+                  READ
+                </Text>
+              </TouchableOpacity>
             </View>
           </View>
-        </View>
+
+          <View
+            style={{
+              marginHorizontal: 16,
+              position: 'relative',
+              borderBottomWidth: 1,
+              borderBottomColor: '#E8E9ED',
+              paddingBottom: 16,
+              marginBottom: 20,
+            }}>
+            <Image
+              source={require('./assets/dummy/food-banner.jpg')}
+              style={{width: '100%', height: 170, borderRadius: 7}}
+            />
+            <View
+              style={{
+                backgroundColor: 'black',
+                width: '100%',
+                height: 170,
+                borderRadius: 7,
+                opacity: 0.2,
+                position: 'absolute',
+              }}></View>
+            <View
+              style={{
+                position: 'absolute',
+                width: 60,
+                height: 15,
+                top: 16,
+                left: 16,
+              }}>
+              <Image
+                source={require('./assets/logo/white.png')}
+                style={{
+                  width: undefined,
+                  height: undefined,
+                  resizeMode: 'contain',
+                  flex: 1,
+                }}
+              />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                position: 'absolute',
+                bottom: 16,
+                left: 0,
+                width: '100%',
+                paddingHorizontal: 19,
+                paddingBottom: 16,
+              }}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    marginBottom: 8,
+                  }}>
+                  Free GO-FOOD voucher
+                </Text>
+                <Text style={{fontSize: 12, fontWeight: '400', color: 'white'}}>
+                  Quick, before they run out!
+                </Text>
+              </View>
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flex: 1,
+                }}>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: '#61A756',
+                    paddingHorizontal: 12,
+                    paddingVertical: 11,
+                    borderRadius: 4,
+                    alignSelf: 'flex-end',
+                  }}>
+                  <Text
+                    style={{color: 'white', fontSize: 13, fontWeight: 'bold'}}>
+                    GET VOUCHER
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </View>
 
       <View style={styles.navMenuContainer}>
@@ -396,6 +414,27 @@ const styles = StyleSheet.create({
   gopayIconText: {
     color: 'white',
     fontSize: 13,
+  },
+
+  featureContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  featureIconBox: {
+    borderWidth: 1,
+    width: 58,
+    height: 58,
+    borderColor: '#EFEFEF',
+    borderRadius: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 6,
+  },
+  featureTextStyle: {
+    fontSize: 11,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 
   navMenu: {
